2650375c45135fa53201d93daa1bd08e47edeff0
unknown <juan23davila@gmail.com>
2019-02-16 21:20:07 -0500

    Se agrega las funcionalidades de deposito, extracción y cambio de límite de extracción, pero no se habilita los botones de la interfaz de usuario.

--- js/homebanking.js
+++ js/homebanking.js
@@ -11,17 +11,39 @@ window.onload = function() {
 }
 
 
-//Funciones que tenes que completar
+/*** Funciones que tenes que completar ***/
+//Se modifica el límite de dinero que un usuario puede extraer
 function cambiarLimiteDeExtraccion() {
-
+    var newlimitExtracc = parseInt(prompt("Indique cual es el nuevo límite de extracción."));
+    limiteExtraccion = newlimitExtracc;
+    actualizarLimiteEnPantalla();
+    alert("Su nuevo límite de extracción es de: "+limiteExtraccion);
 }
 
+//Encargada de disminuir el saldo de la cuenta de acuerdo al dinero retirado
 function extraerDinero() {
-
+    var dineroExtraer = parseInt(prompt("Por favor ingrese el monto de dinero que desea extraer."));
+    var saldoAnterior = saldoCuenta;
+    restarDinero(dineroExtraer);
+    actualizarSaldoEnPantalla();
+    alert(
+        "Saldo anterior: "+saldoAnterior+"\n"+
+        "Retiro de: "+dineroExtraer+"\n"+ 
+        "Nuevo saldo: "+ saldoCuenta
+    );
 }
 
+//Incrementa el dinero que se tiene en la cuenta
 function depositarDinero() {
-
+    var dineroDepositar = parseInt(prompt("Por favor ingrese el monto de dinero que desea depositar."));
+    var saldoAnterior = saldoCuenta;
+    sumarDinero(dineroDepositar);
+    actualizarSaldoEnPantalla();
+    alert(
+        "Saldo anterior: "+saldoAnterior+"\n"+
+        "Deposito: "+dineroDepositar+"\n"+ 
+        "Nuevo saldo: "+ saldoCuenta
+    );
 }
 
 function pagarServicio() {
@@ -36,7 +58,18 @@ function iniciarSesion() {
 
 }
 
-//Funciones que actualizan el valor de las variables en el HTML
+/*** Funciones nuevas solicitadas por el curso ***/
+//Funcion para sumar dinero
+function sumarDinero(dinero){
+    saldoCuenta += dinero;
+}
+
+//Funcion para restar dinero
+function restarDinero(dinero){
+    saldoCuenta -= dinero;
+}
+
+/*** Funciones que actualizan el valor de las variables en el HTML ***/
 function cargarNombreEnPantalla() {
     document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
 }
